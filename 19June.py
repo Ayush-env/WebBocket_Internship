a0e79763834009bb74e79c6cb8156004da73e788
Ayush-env <dayush922@gmail.com>
2026-06-20 10:33:29 +0530

    Adding new files

---
+++ 19June.py
@@ -0,0 +1,39 @@
+#Loop in python
+
+#1 for loop:
+list = [1, 2, 3, 4, 5]
+
+# for i in list:
+#     print(i)
+
+#s = "Hello World"
+#for i in s:
+# print(i)
+
+#for i in range(1,10):
+# print(i)
+
+#while loop
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# Q1) While Loop
+# print("First 10 natural numbers using while loop:")
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# print("\n")
+
+# # Q2) For Loop
+# print("First 10 natural numbers using for loop:")
+# for i in range(1, 11):
+#     print(i)
+
+#Nested Loop
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, j)
--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("Hello World")
+#Loop in python
+
+#1 for loop:
+list = [1, 2, 3, 4, 5]
+
+for i in list:
+    print(i)
+
+
+
